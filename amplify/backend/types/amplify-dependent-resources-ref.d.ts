eaf2f5ac4fe71cede2c600259ddf61e0c43a3802
DevineniTarun <devinenisaitarun@gmail.com>
2021-11-30 15:25:07 -0600

    final changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "personaldrive": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "personaldrivedata": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
